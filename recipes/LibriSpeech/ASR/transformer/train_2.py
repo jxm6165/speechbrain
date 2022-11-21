3f09edd70cd24b2a3817fe22a16b4414758b496f
jxm6165 <35394963+jxm6165@users.noreply.github.com>
2022-11-21 11:28:49 -0500

    Update train_2.py

--- recipes/LibriSpeech/ASR/transformer/train_2.py
+++ recipes/LibriSpeech/ASR/transformer/train_2.py
@@ -286,7 +286,7 @@ class ASR(sb.core.Brain):
                 # anneal lr every update
                 self.hparams.noam_annealing(self.optimizer)
         else:
-            outputs = self.compute_forward(batch, sb.Stage.TRAIN)
+            outputs = self.compute_forward(batch, batch_idx, sb.Stage.TRAIN)
             loss = self.compute_objectives(outputs, batch, sb.Stage.TRAIN)
             (loss / self.grad_accumulation_factor).backward()
             if should_step:
