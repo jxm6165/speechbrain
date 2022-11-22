0de39789135cdaeb2cbd4013e14372e87df461b9
jxm6165 <35394963+jxm6165@users.noreply.github.com>
2022-11-21 20:51:39 -0500

    Update InterleaveFormerSegASR.py

--- speechbrain/lobes/models/transformer/InterleaveFormerSegASR.py
+++ speechbrain/lobes/models/transformer/InterleaveFormerSegASR.py
@@ -16,8 +16,10 @@ from speechbrain.lobes.models.transformer.InterleaveFormerSeg import (
     NormalizedEmbedding,
 )
 from speechbrain.nnet.activations import Swish
+import logging
 # from speechbrain.dataio.dataio import length_to_mask
 
+logger = logging.getLogger(__name__)
 
 class InterleaveFormerASR(InterleaveFormerInterface):
     """This is an implementation of InterleaveFormer model for ASR.
@@ -187,7 +189,10 @@ class InterleaveFormerASR(InterleaveFormerInterface):
             causal_mask, # causal for text, non-causal for audio.
         ) = self.make_masks(rebatch_sample, modalities, audio_stats, text_stats)
         # repeat each sample's causal mask by num_heads # of times.
+        logger.warn(causal_mask.shape)
+        # repeat each sample's causal mask by num_heads # of times.
         causal_mask = torch.cat( causal_mask ).repeat_interleave(repeats = self.nhead, dim = 0).to(rebatch_sample.device)
+        logger.warn(causal_mask.shape)
         padding_mask = padding_mask.to(rebatch_sample.device)
         
         # encoded_output is bi-modality learned representation.
